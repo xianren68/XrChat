eedbb93cf640669e42bfdfaa30ac3158ff296577
xianren <xianren711@qq.com>
2024-04-04 10:42:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <Aside ></Aside>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Aside from './components/Aside.vue'
+
+</script>
+
+<style scoped>
+.app {
+  box-sizing: border-box;
+  display: flex;
+  padding: 20px;
+  height: 100%;
+  width: 100%
+}
+</style>
